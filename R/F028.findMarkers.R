f3a61bd9e93d4fbbc12169534b7807cc52ad5d10
Khodadadi-Jamayran <rezakj@users.noreply.github.com>
2019-03-11 17:18:46 -0400

    update

---
+++ R/F028.findMarkers.R
@@ -0,0 +1,127 @@
+#' Find marker genes for each cluster
+#'
+#' This function takes an object of class iCellR and performs differential expression (DE) analysis to find marker genes for each cluster.
+#' @param x An object of class iCellR.
+#' @param fold.change A number that designates the minimum fold change for out put, default = 2.
+#' @param padjval Minimum adjusted p value for out put, default = 0.1.
+#' @param Inf.FCs If set to FALSE the infinite fold changes would be filtered from out put, default = FALSE.
+#' @param uniq If set to TRUE only genes that are a marker for only one cluster would be in the out put, default = TRUE.
+#' @param positive If set to FALSE both the up regulated (positive) and down regulated (negative) markers would be in the out put, default = FALSE.
+#'
+#' @return An object of class iCellR
+#' @examples
+#' \dontrun{
+#' marker.genes <- find.markers(my.obj,fold.change = 2,padjval = 0.1,uniq = T)
+#' }
+#'
+#' @export
+findMarkers <- function (x = NULL,
+          fold.change = 2,
+          padjval = 0.1,
+          Inf.FCs = F,
+          uniq = F,
+          positive = TRUE) {
+  if ("iCellR" != class(x)[1]) {
+    stop("x should be an object of class iCellR")
+  }
+  ###########
+#  x <- clust.avg.exp(x)
+  dat <- x@main.data
+  # get cluster data
+  DATA <- x@best.clust
+  ############## set wich clusters you want as condition 1 and 2
+  MyClusts <- as.numeric(unique(DATA$clusters))
+############################### loop start
+  for (i in MyClusts) {
+    Noi <- MyClusts[-which((MyClusts) %in% i)]
+    Table=DATA
+    Cluster0 <- row.names(subset(Table, Table$clusters %in% i))
+    Cluster1 <- row.names(subset(Table, Table$clusters %in% Noi))
+    ############## Filter
+    cond1 <- dat[,Cluster0]
+    cond2 <- dat[,Cluster1]
+    ### merge both for pval length not matching error
+    mrgd <- cbind(cond1,cond2)
+#    mrgd <- merge(cond1, cond2, by="row.names")
+#    row.names(mrgd) <- mrgd$Row.names
+#    mrgd <- mrgd[,-1]
+    mrgd <- data.matrix(mrgd)
+    # mean
+    meansCond1 <- apply(cond1, 1, function(cond1) {mean(cond1)})
+    meansCond2 <- apply(cond2, 1, function(cond2) {mean(cond2)})
+    baseMean <- apply(mrgd, 1, function(mrgd) {mean(mrgd)})
+    baseSD <- apply(mrgd, 1, function(mrgd) {sd(mrgd)})
+    # FC
+    FC <- meansCond1/meansCond2
+    FC.log2 <- log2(FC)
+    # dims
+    Cond1_Start <- 1
+    Cond1_End <- dim(cond1)[2]
+    Cond2_Start <- dim(cond1)[2] + 1
+    Cond2_End <- dim(cond1)[2] + dim(cond2)[2]
+    # filter
+    if (positive == T) {
+      FiltData <- subset(FC.log2, FC.log2 > log2(fold.change))
+    } else {
+      FiltData <- subset(FC.log2, FC.log2 < -log2(fold.change) | FC.log2 > log2(fold.change))
+    }
+    mrgd <- subset(mrgd, row.names(mrgd) %in% row.names(as.data.frame(FiltData)))
+    # pval
+    Pval <- apply(mrgd, 1, function(mrgd) {
+      t.test(x = mrgd[Cond1_Start:Cond1_End], y = mrgd[Cond2_Start:Cond2_End])$p.value
+    })
+    # padj
+    FDR <- p.adjust(Pval)
+    # combine
+    Stats <- cbind(
+      baseMean = baseMean,
+      baseSD = baseSD,
+      MeanCond1 = meansCond1,
+      MeanCond2 = meansCond2,
+      FC=FC,
+      FC.log2=FC.log2)
+    colnames(Stats) <- c("baseMean","baseSD","AvExpInCluster", "AvExpInOtherClusters","foldChange","log2FoldChange")
+    # cbind pvals
+    clusters <- rep(i,length(FDR))
+    # make cluster names
+    Stats1 <- cbind(
+      pval = Pval,
+      padj = FDR,
+      clusters = clusters)
+    # filter
+    Stats <- as.data.frame(Stats)
+    Stats1 <- as.data.frame(Stats1)
+    Stats1 <- subset(Stats1, Stats1$padj < padjval)
+    # merge both pvals and stats
+    mrgdall <- merge(Stats, Stats1, by="row.names")
+    row.names(mrgdall) <- mrgdall$Row.names
+    mrgdall <- mrgdall[,-1]
+    # get avrage data
+    AvData <- x@clust.avg
+    row.names(AvData) <- AvData$gene
+    mrgdall <- merge(mrgdall, AvData, by="row.names")
+    row.names(mrgdall) <- mrgdall$Row.names
+    mrgdall <- mrgdall[,-1]
+    # make it an object
+    DatNmaes=paste("DATAcluster",i,sep="_")
+    eval(call("<-", as.name(DatNmaes), mrgdall))
+  }
+############################### loop end
+# merge all
+  filenames <- ls(pattern="DATAcluster_")
+  datalist <- mget(filenames)
+  df <- do.call("rbind", datalist)
+  row.names(df) <- make.names(df$gene, unique=TRUE)
+  df <- subset(df, gene != "NA")
+####
+  if (uniq == T) {
+    df <- df[!duplicated(df$gene), ]
+  }
+  if (Inf.FCs == FALSE) {
+    df <- subset(df, log2FoldChange != Inf)
+    df <- subset(df, log2FoldChange != Inf & log2FoldChange != -Inf)
+  }
+  df <- df[order(df$log2FoldChange,decreasing = T),]
+  df <- df[order(df$clusters,decreasing = F),]
+  return(df)
+}
---
+++ R/F041.clono.plot.R
@@ -0,0 +1,136 @@
+#' Make 2D and 3D scatter plots for clonotypes.
+#'
+#' This function takes an object of class iCellR and provides plots for clonotypes.
+#' @param x An object of class iCellR.
+#' @param clono A clonotype name to be plotted, default = 1.
+#' @param plot.data.type Choose from "tsne" and "pca", default = "tsne".
+#' @param clust.dim 2 for 2D plots and 3 for 3D plots, default =  2.
+#' @param cell.size A number for the size of the points in the plot, default = 1.
+#' @param cell.colors Colors for heat mapping the points in "scatterplot", default = c("gray","red").
+#' @param back.col A color for the plot background, default = "black".
+#' @param cell.transparency Color transparency for points, default = 0.5.
+#' @param interactive If set to TRUE an intractive HTML file will be created, default = TRUE.
+#' @param out.name If "interactive" is set to TRUE, the out put name for HTML, default = "plot".
+#' @return An object of class iCellR.
+#' @examples
+#' \dontrun{
+#' clono.plot(my.obj,
+#'             plot.data.type = "tsne",
+#'             clono = 1,
+#'             clust.dim = 2,
+#'             cell.size = 1,
+#'             cell.colors = c("red","gray"),
+#'             cbox.cell.col = "black",
+#'             back.col = "white",
+#'             interactive = T,
+#'             cell.transparency = 0.5,
+#'             out.name = "tSNE_3D_clusters")
+#' }
+#' @import ggpubr
+#' @export
+clono.plot <- function (x = NULL,
+                       plot.data.type = "tsne",
+                       clono = 1,
+                       clust.dim = 2,
+                       cell.size = 1,
+                       cell.colors = c("red","gray"),
+                       box.cell.col = "black",
+                       back.col = "white",
+                       cell.transparency = 0.5,
+                       interactive = TRUE,
+                       out.name = "plot") {
+  if ("iCellR" != class(x)[1]) {
+    stop("x should be an object of class iCellR")
+  }
+  ##### get cluster data
+  # 2 dimentions
+  if (clust.dim == 2) {
+    if (plot.data.type == "tsne") {
+      MyTitle = "tSNE Plot"
+      DATA <- x@tsne.data
+    }
+    if (plot.data.type == "pca") {
+      MyTitle = "PCA Plot"
+      DATA <- x@pca.data
+    }
+    if (plot.data.type == "umap") {
+      MyTitle = "UMAP Plot"
+      DATA <- x@umap.data
+    }
+  }
+  # 3 dimentions
+  if (clust.dim == 3) {
+    if (plot.data.type == "tsne") {
+      MyTitle = "3D tSNE Plot"
+      DATA <- x@tsne.data.3d
+    }
+    if (plot.data.type == "pca") {
+      MyTitle = "3D PCA Plot"
+      DATA <- x@pca.data
+    }
+    if (plot.data.type == "dst") {
+      MyTitle = "3D DST Plot"
+      DATA <- x@diff.st.data
+    }
+  }
+  ## clonotype
+      colono <- unique(x@vdj.data[1:2])
+      row.names(colono) <- colono$barcode
+      colono$raw_clonotype_id <- gsub("clonotype"," ", colono$raw_clonotype_id)
+      colono <- colono[1]
+      colnames(colono) <- c("Clonotypes")
+      colonoData <- merge(DATA,colono, by="row.names", all.x=T, all.y=F)
+      colonoData$Clonotypes <- gsub( " ", "", colonoData$Clonotypes)
+      colonoData$Clonotypes[is.na(colonoData$Clonotypes)] <- "NA"
+      colonoData$Clonotypes[colonoData$Clonotypes != clono] <- "NA"
+      colonoData$Clonotypes[colonoData$Clonotypes == clono] <- clono
+      DATA <- colonoData
+      row.names(DATA) <- DATA$Row.names
+      DATA <- DATA[,-1]
+      DATA <- (DATA[order(DATA$Clonotypes, decreasing = T),])
+      clonotype <- factor(DATA$Clonotypes)
+    # plot 2d
+    if (clust.dim == 2) {
+      if (interactive == F) {
+        myPLOT <- ggplot(DATA, aes(DATA[,1], y = DATA[,2],col=clonotype,
+                                   text = row.names(DATA))) +
+          geom_point(size = cell.size, alpha = cell.transparency) +
+          xlab("Dim1") +
+          ylab("Dim2") +
+          ggtitle(paste(MyTitle, "(clonotype",clono, ")")) +
+          theme(legend.position = "none") +
+          scale_color_manual(values = cell.colors) +
+          theme(panel.background = element_rect(fill = back.col, colour = "black"),
+                panel.grid.major = element_blank(), panel.grid.minor = element_blank(),
+                legend.key = element_rect(fill = back.col))
+      } else {
+        myPLOT <- ggplot(DATA, aes(DATA[,1], y = DATA[,2],
+                                   text = row.names(DATA), color = clonotype)) +
+          geom_point(size = cell.size, alpha = cell.transparency) +
+          scale_color_manual(values = cell.colors) +
+          xlab("Dim1") +
+          ylab("Dim2") +
+          ggtitle(MyTitle) +
+          theme_bw()
+      }
+    }
+    # plot 3d
+    if (clust.dim == 3) {
+      myPLOT <- plot_ly(DATA, x = DATA[,1], y = DATA[,2], z = DATA[,3], text = row.names(DATA),
+                        color = clonotype, opacity = cell.transparency, marker = list(size = cell.size + 2)) %>%
+        layout(DATA, x = DATA[,1], y = DATA[,2], z = DATA[,3]) %>%
+        layout(plot_bgcolor = back.col) %>%
+        layout(paper_bgcolor = back.col) %>%
+        layout(title = MyTitle,
+               scene = list(xaxis = list(title = "Dim1"),
+                            yaxis = list(title = "Dim2"),
+                            zaxis = list(title = "Dim3")))
+    }
+  # return
+  if (interactive == T) {
+    OUT.PUT <- paste(out.name, ".html", sep="")
+    htmlwidgets::saveWidget(ggplotly(myPLOT), OUT.PUT)
+  } else {
+    return(myPLOT)
+  }
+}
