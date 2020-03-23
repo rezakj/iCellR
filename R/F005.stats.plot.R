9534cf9b6ebb000745f83f0b7925a6c65cc0350f
Khodadadi-Jamayran <khodaa01@KHODAA01-MBP15T.home>
2020-03-23 19:38:06 -0400

    1.3.4

--- R/F005.stats.plot.R
+++ R/F005.stats.plot.R
@@ -64,7 +64,7 @@ if (do == 2) {
     geom_violin(trim=FALSE, col = "black", alpha = cell.transparency) +
     geom_boxplot( fill = box.color, col = "green", notch = FALSE, outlier.shape = NA, alpha = cell.transparency) +
     xlab("mito.percent") + ylab("percent of mito genes per cell") +
-    stat_summary(fun.y=mean, geom="point", size=2, color="black") +
+    stat_summary(fun=mean, geom="point", size=2, color="black") +
     theme_bw() + theme(axis.text.x=element_text(angle=90))
     # nGenes
   nGenes.plot <- ggplot(DATA,aes(y=nGenes,x=col.legend)) +
@@ -72,7 +72,7 @@ if (do == 2) {
     geom_violin(trim=FALSE, col = "black", alpha = cell.transparency) +
     geom_boxplot( fill = box.color, col = box.line.col, notch = FALSE, outlier.shape = NA, alpha = cell.transparency) +
     xlab("nGenes") + ylab("number of genes per cell") +
-    stat_summary(fun.y=mean, geom="point", size=2, color="black") +
+    stat_summary(fun=mean, geom="point", size=2, color="black") +
     theme_bw() + theme(axis.text.x=element_text(angle=90))
     # UMIs
   UMIsplot <- ggplot(DATA,aes(y=UMIs,x=col.legend)) +
@@ -80,7 +80,7 @@ if (do == 2) {
     geom_violin(trim=FALSE, col = "black", alpha = cell.transparency) +
     geom_boxplot( fill = box.color, col = box.line.col, notch = FALSE, outlier.shape = NA, alpha = cell.transparency) +
     xlab("UMIs") + ylab("number of UMIs per cell") +
-    stat_summary(fun.y=mean, geom="point", size=2, color="black") +
+    stat_summary(fun=mean, geom="point", size=2, color="black") +
     theme_bw() + theme(axis.text.x=element_text(angle=90))
   # s.phase
   s.plot <- ggplot(DATA,aes(y=S.phase.probability,x=col.legend)) +
@@ -88,7 +88,7 @@ if (do == 2) {
     geom_violin(trim=FALSE, col = "black", alpha = cell.transparency) +
     geom_boxplot( fill = box.color, col = box.line.col, notch = FALSE, outlier.shape = NA, alpha = cell.transparency) +
     xlab("S phase") + ylab("S phase probability") +
-    stat_summary(fun.y=mean, geom="point", size=2, color="black") +
+    stat_summary(fun=mean, geom="point", size=2, color="black") +
     theme_bw() + theme(axis.text.x=element_text(angle=90))
   # g2m.phase.probability
   g2m.plot <- ggplot(DATA,aes(y=g2m.phase.probability,x=col.legend)) +
@@ -96,7 +96,7 @@ if (do == 2) {
     geom_violin(trim=FALSE, col = "black", alpha = cell.transparency) +
     geom_boxplot( fill = box.color, col = box.line.col, notch = FALSE, outlier.shape = NA, alpha = cell.transparency) +
     xlab("G2 and M phase") + ylab("G2 and M phase probability") +
-    stat_summary(fun.y=mean, geom="point", size=2, color="black") +
+    stat_summary(fun=mean, geom="point", size=2, color="black") +
     theme_bw() + theme(axis.text.x=element_text(angle=90))
 # scatter plots
   if (col.legend[1] == ".") {
